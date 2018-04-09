9c8a624dc645b2d57ffc71cba2198293cf936922
Fedor Paretsky <fedorp1@uw.edu>
2018-04-08 18:51:19 -0700

    Add refresh interval for new data in spotify.js.

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -13,24 +12,26 @@ $(document).ready(function () {
         }
     }
 
-    $.ajax(settings).done(function (response) {
-        if (response['is_playing']) {
-            var contents = "Right now, I'm listening to <strong>" + response['item'].name + "</strong> by ";
-            contents += "<strong>" + response['item']['artists'][0].name + "</strong>";
-            if (response['item'].artists.length == 2) {
-                contents += " and <strong>" + response['item']['artists'][1].name + "</strong>";
-            } else if (response['item'].artists.length > 2) {
-                for (var artistNum = 1; artistNum < response['item'].artists.length; artistNum++) {
-                    if (artistNum == response['item'].artists.length-1) {
-                        contents += ", and <strong>" + response['item']['artists'][artistNum].name + "</strong>";    
-                    } else {
-                        contents += ", <strong>" + response['item']['artists'][artistNum].name + "</strong>";
+    setInterval(function () {
+        $.ajax(settings).done(function (response) {
+            if (response['is_playing']) {
+                var contents = "Right now, I'm listening to <strong>" + response['item'].name + "</strong> by ";
+                contents += "<strong>" + response['item']['artists'][0].name + "</strong>";
+                if (response['item'].artists.length == 2) {
+                    contents += " and <strong>" + response['item']['artists'][1].name + "</strong>";
+                } else if (response['item'].artists.length > 2) {
+                    for (var artistNum = 2; artistNum < response['item'].artists.length; artistNum++) {
+                        if (artistNum == response['item'].artists.length - 1) {
+                            contents += ", and <strong>" + response['item']['artists'][artistNum].name + "</strong>";
+                        } else {
+                            contents += ", <strong>" + response['item']['artists'][artistNum].name + "</strong>";
+                        }
                     }
+                    contents += ". "
                 }
-                contents += ". "
+                contents += "Listen to it <a href=" + response['item'].external_urls.spotify + ">here</a>."
+                $('.listeningto').html(contents);
             }
-            contents += "Listen to it <a href=" + response['item'].external_urls.spotify + ">here</a>."
-            $('.listeningto').html(contents);
-        }
-    });
+        });
+    }, 2000);
 });
