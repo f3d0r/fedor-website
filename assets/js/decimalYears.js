79ebf2a96e2af59073a83fdd957eb1883affd0f9
Fedor Paretsky <fedorp1@uw.edu>
2018-04-11 11:56:33 -0700

    Add age update, update section headers

---
+++ assets/js/decimalYears.js
@@ -0,0 +1,11 @@
+var birthday = new Date(1997, 11, 12, 6, 15, 0, 0);
+
+$(document).ready(function () {
+    setInterval(calculateDecimalYear, 75);
+});
+
+function calculateDecimalYear() {
+    var now = Date.now();
+    var decimalYears = (now - birthday) / 31556952000;
+    $('.decimalAge').html("I'm " + decimalYears.toFixed(9) + " years old., but I have some experience already. :)");
+}
